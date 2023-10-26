8c9eabc9fc3739cc737e689b5c20bb8aa945bb8a
MjBonk <mayajerndahl@gmail.com>
2023-10-26 20:20:25 +0200

    javascript radio error

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-const mainCont = document.querySelector(".main");
-const mainContHide = document.querySelector(".main--hide");
+const mainContainer = document.querySelector(".main--hide");
+const mainError = document.getElementById("main--error");
 const thankYou = document.querySelector(".thank-you");
-
 const result = document.getElementById("result");
 const radio = document.querySelectorAll("input[name='radio']");
 
-
 function submit() {
 	for (let i = 0; i < radio.length; i += 1) {
-		if (radio[i].checked) {
-			mainContHide.remove();
+		if (radio[i].checked == true) {
+			mainContainer.remove();
 			thankYou.classList.remove("--hidden");
 			result.innerHTML =
-			"You selected " + radio[i].value + " out of 5";
+				"You selected " + radio[i].value + " out of 5";
 		} else {
-			//do nothing
+			mainError.style.animationPlayState = "running";
 		}
 	}
 }
-
-/* const error = [
-	document.querySelector(".main--error"),
-	document.querySelector("radio--error"),
-]; */
-/* radio.forEach((Element) => console.log(Element.value)); */
-/* array1.forEach((element) => console.log(element)); */
